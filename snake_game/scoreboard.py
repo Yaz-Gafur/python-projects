c0e0b49fd98c4ca6a7d98c41ff6b7a70d840ff1a
Yaz-Gafur <140863760+Yaz-Gafur@users.noreply.github.com>
2024-12-07 11:59:33 +0500

    Update scoreboard.py

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -7,6 +7,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt", mode = "r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -14,14 +16,18 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}  High score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-        self.score = 0
+            with open("data.txt", mode = "w") as file:
+                file.write(f"{self.high_score}")
+            self.score = 0
+            self.update_score()
 
     def increase_score(self):
-        self.score += 1
         self.clear()
+        self.score += 1
         self.update_scoreboard()
